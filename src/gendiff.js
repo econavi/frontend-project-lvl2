0a15f567348c9016ccd181495934274008a0f6ec
Vladimir Kochenov <kochenov.vl@gmail.com>
2020-03-04 14:34:19 +0300

    Add arguments for paths of compared files and output type option

--- src/gendiff.js
+++ src/gendiff.js
@@ -5,4 +5,14 @@ const program = new commander.Command();
 
 program.version('0.0.1');
 program.description('Compares two configuration files and shows a difference.');
+
+program.option('-f, --format [type]', 'output format');
+
+program.arguments('<firstConfig> <secondConfig>');
+program.action((firstConfig, secondConfig) => {
+  console.log(firstConfig, secondConfig);
+});
+
 program.parse(process.argv);
+
+if (program.format) console.log(`output format - ${program.format}`);
